39a0cf5ca8c0b3dcb2ba449135cf7cd87b72f17e
rin1999 <okarinvc@gmail.com>
2024-04-21 08:36:13 +0900

    temporary save of pulseheightfit_all.C while improving fit. A lot of debugging information

--- ana/pulseheightfit_all.C
+++ ana/pulseheightfit_all.C
@@ -8,6 +8,7 @@
 #include <TF1.h>
 #include <TCanvas.h>
 #include <TLine.h>
+#include <TBox.h>
 #include <TLatex.h>
 #include <TPDF.h>
 #include <TRegexp.h>
@@ -44,7 +45,7 @@ double analyzeRootFile(const char* fileName, Int_t canvasDimentionX, Int_t canva
     // Create a histogram to store the log-transformed y values
     Double_t xRangeMax = TMath::Log10(graph->GetYaxis()->GetXmax())+0.5;
     xRangeMax = 8.0;
-    TH1F* hist = new TH1F("hist", "Log Transformed Y Values Histogram", 100, 0, xRangeMax);
+    TH1F* hist = new TH1F("hist", "Log Transformed Y Values Histogram", 50, 0, xRangeMax);
 
     // Fill the histogram with the log-transformed y values of the TGraph
     if (DEBUG) {
@@ -63,37 +64,37 @@ double analyzeRootFile(const char* fileName, Int_t canvasDimentionX, Int_t canva
     TF1* fitFunc = new TF1("fitFunc", "gaus(0) + gaus(3) + gaus(6) + [9]", hist->GetXaxis()->GetXmin(), hist->GetXaxis()->GetXmax());
 
     // Set the fitting range (e.g., from -1 to 1 in log scale)
-    double fitRangeMin = 3.0; // Set the minimum value for fitting region
+    double fitRangeMin = 2.0; // Set the minimum value for fitting region
     double fitRangeMax = 8.0;  // Set the maximum value for fitting region
     fitFunc->SetRange(fitRangeMin, fitRangeMax);    
 
     // Set initial parameters for the first Gaussian
-    fitFunc->SetParameter(0, 4);          // Amplitude Initial Value
-    fitFunc->SetParameter(1, 7);          // Mean Initial Value
+    fitFunc->SetParameter(0, 4.0);        // Amplitude Initial Value
+    fitFunc->SetParameter(1, 7.2);        // Mean Initial Value
     fitFunc->SetParameter(2, 0.1);        // Sigma Initial Value
     fitFunc->SetParLimits(0, 2, 8);       // Amplitude Range
-    fitFunc->SetParLimits(1, 5, 8);       // Mean Range
-    fitFunc->SetParLimits(2, 0.001, 0.2); // Sigma Range
+    fitFunc->SetParLimits(1, 5.0, 8);     // Mean Range
+    fitFunc->SetParLimits(2, 0.03, 0.2);  // Sigma Range
 
     // Set initial parameters for the second Gaussian
-    fitFunc->SetParameter(3, 4);          // Amplitude
-    fitFunc->SetParameter(4, 5);          // Mean
+    fitFunc->SetParameter(3, 4.0);        // Amplitude
+    fitFunc->SetParameter(4, 5.0);        // Mean
     fitFunc->SetParameter(5, 0.1);        // Sigma
     fitFunc->SetParLimits(3, 2, 8);       // Amplitude Range
-//    fitFunc->SetParLimits(4, 5, 8);       // Mean Range
-    fitFunc->SetParLimits(5, 0.001, 0.2); // Sigma Range
+    fitFunc->SetParLimits(4, 4.5, 6);     // Mean Range
+    fitFunc->SetParLimits(5, 0.1, 0.4);   // Sigma Range
 
     // Set initial parameters for the third Gaussian
-    fitFunc->SetParameter(6, 3);          // Amplitude
-    fitFunc->SetParameter(7, 3);          // Mean
+    fitFunc->SetParameter(6, 3.0);        // Amplitude
+    fitFunc->SetParameter(7, 3.5);        // Mean
     fitFunc->SetParameter(8, 0.2);        // Sigma
     fitFunc->SetParLimits(6, 1.5, 8);     // Amplitude Range
-//    fitFunc->SetParLimits(7, 5, 8);       // Mean Range
-    fitFunc->SetParLimits(8, 0.001, 0.2); // Sigma Range
+    fitFunc->SetParLimits(7, 2.5, 4);     // Mean Range
+    fitFunc->SetParLimits(8, 0.1, 0.5);   // Sigma Range
 
     // Set initial parameters for the constant background
-    fitFunc->SetParameter(9, 0.2);        // Constant Initial Value
-    fitFunc->SetParLimits(9, 0.01, 0.5);  // Constant Range
+    fitFunc->SetParameter(9, 0.1);        // Constant Initial Value
+    fitFunc->SetParLimits(9, 0.01, 0.1);  // Constant Range
 
     // Display the histogram
     TCanvas* canvas = new TCanvas("canvas", "Analysis Canvas", 1000, 1000);
@@ -103,19 +104,42 @@ double analyzeRootFile(const char* fileName, Int_t canvasDimentionX, Int_t canva
     gPad->SetLogy();
     graph->Draw();
     canvas->cd(2);
+    Double_t hMaximum = hist->GetMaximum();
+    hist->GetXaxis()->SetTitle("Log10 Count Rate");
+    hist->GetYaxis()->SetTitle("Count");
     hist->Draw();
     //hist->Fit(fitFunc, "R", fitRangeMin, fitRangeMax);
-    hist->Fit(fitFunc, "R");
-    canvas->cd(2);
-    canvas->Draw();
-    
+    hist->Fit(fitFunc, "LR");
     // Get the count rates of 1 p.e., 2 p.e., and 3 p.e.
+    double countRate1peLog10Amp = fitFunc->GetParameter(0);
+    double countRate2peLog10Amp = fitFunc->GetParameter(3);
+    double countRate3peLog10Amp = fitFunc->GetParameter(6);
     double countRate1peLog10 = fitFunc->GetParameter(1);
     double countRate2peLog10 = fitFunc->GetParameter(4);
     double countRate3peLog10 = fitFunc->GetParameter(7);
-    messagestring.Form("The first 3 flat count rates (in log10) detected: %f %f %f",
-                       countRate1peLog10, countRate2peLog10, countRate3peLog10);
+    double countRate1peLog10Sigma = fitFunc->GetParameter(2);
+    double countRate2peLog10Sigma = fitFunc->GetParameter(5);
+    double countRate3peLog10Sigma = fitFunc->GetParameter(8);
+    messagestring.Form("The first 3 flat count rates (in log10) detected: %f±%f %f±%f  %f±%f",
+                       countRate1peLog10, countRate1peLog10Sigma, 
+                       countRate2peLog10, countRate2peLog10Sigma,
+                       countRate3peLog10, countRate3peLog10Sigma);
     std::cout << messagestring << std::endl;
+
+    TLatex* textFitResult1pe = new TLatex(0.5, hMaximum-0.2,
+                                          Form("1pe peak %4.3f %4.3f %4.3f", countRate1peLog10Amp, countRate1peLog10, countRate1peLog10Sigma));
+    textFitResult1pe->Draw();
+    TLatex* textFitResult2pe = new TLatex(0.5, hMaximum-0.9,
+                                          Form("2pe peak %4.3f %4.3f %4.3f", countRate2peLog10Amp, countRate2peLog10, countRate2peLog10Sigma));
+    textFitResult2pe->Draw();
+    TLatex* textFitResult3pe = new TLatex(0.5, hMaximum-1.6,
+                                          Form("3pe peak %4.3f %4.3f %4.3f", countRate3peLog10Amp, countRate3peLog10, countRate3peLog10Sigma));
+    textFitResult3pe->Draw();
+    
+    
+    canvas->cd(2);
+    canvas->Draw();
+    
     
     // Threshold-vs-CountRate plot Analysis
     int numPoints = graph->GetN();
@@ -126,6 +150,16 @@ double analyzeRootFile(const char* fileName, Int_t canvasDimentionX, Int_t canva
     double TransitionEdge2pe3pe = 0.;
     int Ninterval12 = 0;
     int Ninterval23 = 0;
+    double DACMin = 0;
+    double DACMax = 500;
+
+    std::ofstream logFile;
+    TString logFileName = Form("pulseheightfit_all-%03i-%02i-%03i.log", iCIRASAME, iASIC, iChannel);
+    logFile.open(logFileName);
+    if (!logFile.is_open()) {
+       std::cout << "Error: Unable to open file " << logFileName << std::endl;
+    }
+    
     if (DEBUG) {
        std::cout << "Threshold-vs-CountRate Raw Data" << std::endl;
     }
@@ -137,31 +171,42 @@ double analyzeRootFile(const char* fileName, Int_t canvasDimentionX, Int_t canva
         } else {
            log10y = TMath::Log10(y);
         }
-        if (DEBUG) {
-           messagestring.Form("   %03f     %8.1f    %8.3f", x, y, log10y);
+        if (DEBUG==2) {
+           messagestring.Form("   %8i     %8i    %8.3f", int(x), int(y), log10y);
            std::cout << messagestring << std::endl;
+           logFile << messagestring << std::endl;
         }
 
-        if (TMath::Abs(log10y-countRate1peLog10) < 0.2) {
+        if (TMath::Abs(log10y-countRate1peLog10) < countRate1peLog10Sigma) {
            is1pefound = true;
-           std::cout << " this data point belongs to the 1 photon-equivalent" << std::endl;
-        } else if ((TMath::Abs(log10y-countRate1peLog10) >= 0.2) &&
+           messagestring.Form(" %8i %8i belongs to the 1 photon-equivalent", int(x), int(y));
+           std::cout << messagestring << std::endl;
+           if (DEBUG==2) logFile << messagestring << std::endl;
+        } else if ((TMath::Abs(log10y-countRate1peLog10) >= countRate1peLog10Sigma) &&
                    (is1pefound == true) && (TMath::Abs(log10y-countRate2peLog10) >= 0.2) &&
                    (is2pefound == false)) {
            TransitionEdge1pe2pe += x;
            Ninterval12++;
-           std::cout << "Interval 1 and 2 " << std::endl;
-        } else if ((TMath::Abs(log10y-countRate2peLog10) < 0.2) && (is1pefound == true)) {
+           messagestring.Form(" %8i %8i Transition edge between  1pe and 2pe", int(x), int(y));
+           std::cout << messagestring << std::endl;
+           if (DEBUG==2) logFile << messagestring << std::endl;
+        } else if ((TMath::Abs(log10y-countRate2peLog10) < countRate2peLog10Sigma) && (is1pefound == true)) {
            is2pefound = true;
-           std::cout << " this data point belongs to the 2 photon-equivalent" << std::endl;
-        } else if ((TMath::Abs(log10y-countRate2peLog10) >= 0.2) && (is2pefound == true) &&
-                   (TMath::Abs(log10y-countRate3peLog10) >= 0.2) && (is3pefound == false)) {
+           messagestring.Form(" %8i %8i belongs to the 2 photon-equivalent", int(x), int(y));
+           std::cout << messagestring << std::endl;
+           if (DEBUG==2) logFile << messagestring << std::endl;
+        } else if ((TMath::Abs(log10y-countRate2peLog10) >= countRate2peLog10Sigma) && (is2pefound == true) &&
+                   (TMath::Abs(log10y-countRate3peLog10) >= countRate2peLog10Sigma) && (is3pefound == false)) {
            TransitionEdge2pe3pe += x;
            Ninterval23++;
-           std::cout << "Interval 2 and 3 " << std::endl;
-        } else if ((TMath::Abs(log10y-countRate3peLog10) < 0.2) && (is2pefound == true)) {
+           messagestring.Form(" %8i %8i Transition edge between  2pe and 3pe", int(x), int(y));
+           std::cout << messagestring << std::endl;
+           if (DEBUG==2) logFile << messagestring << std::endl;
+        } else if ((TMath::Abs(log10y-countRate3peLog10) < countRate3peLog10Sigma) && (is2pefound == true)) {
            is3pefound = true;
-           std::cout << " this data point belongs to the 3 photon-equivalent" << std::endl;
+           if (DEBUG==2) messagestring.Form(" %8i %8i belongs to the 3 photon-equivalent", int(x), int(y));
+           std::cout << messagestring << std::endl;
+           logFile << messagestring << std::endl;
         }
     }
     Double_t gain = 0.0;
@@ -179,15 +224,38 @@ double analyzeRootFile(const char* fileName, Int_t canvasDimentionX, Int_t canva
        messagestring.Form("DAC 1pe = %f,  DAC 2pe = %f, Gain = %f", TransitionEdge1pe2pe, TransitionEdge2pe3pe, gain);
        std::cout << messagestring << std::endl;
        canvas->cd(1);
-       TLine* line1 = new TLine(TransitionEdge1pe2pe, 0, TransitionEdge1pe2pe, graphYMax);
-       TLine* line2 = new TLine(TransitionEdge2pe3pe, 0, TransitionEdge2pe3pe, graphYMax);
-       TLine* lineCR1pe = new TLine(0, TMath::Power(10, countRate1peLog10), 500, TMath::Power(10, countRate1peLog10));
-       TLine* lineCR2pe = new TLine(0, TMath::Power(10, countRate2peLog10), 500, TMath::Power(10, countRate2peLog10));
-       TLine* lineCR3pe = new TLine(0, TMath::Power(10, countRate3peLog10), 500, TMath::Power(10, countRate3peLog10));
-       line1->SetLineColor(kRed);
-       line1->Draw();    
-       line2->SetLineColor(kRed);
-       line2->Draw();
+       TLine* lineTE1pe2pe = new TLine(TransitionEdge1pe2pe, 0, TransitionEdge1pe2pe, graphYMax);
+       TLine* lineTE2pe3pe = new TLine(TransitionEdge2pe3pe, 0, TransitionEdge2pe3pe, graphYMax);
+       TLine* lineCR1pe = new TLine(DACMin, TMath::Power(10, countRate1peLog10), DACMax, TMath::Power(10, countRate1peLog10));
+       TLine* lineCR2pe = new TLine(DACMin, TMath::Power(10, countRate2peLog10), DACMax, TMath::Power(10, countRate2peLog10));
+       TLine* lineCR3pe = new TLine(DACMin, TMath::Power(10, countRate3peLog10), DACMax, TMath::Power(10, countRate3peLog10));
+       lineTE1pe2pe->SetLineColor(kRed);
+       lineTE1pe2pe->Draw();    
+       lineTE2pe3pe->SetLineColor(kRed);
+       lineTE2pe3pe->Draw();
+
+       Double_t CR1peTorelanceLow = TMath::Power(10, countRate1peLog10-countRate1peLog10Sigma);
+       Double_t CR1peTorelanceHigh = TMath::Power(10, countRate1peLog10+countRate1peLog10Sigma);
+       TBox *boxCR1pe = new TBox(0, CR1peTorelanceLow, 500, CR1peTorelanceHigh);
+       boxCR1pe->SetFillColorAlpha(kBlue, 0.3); // Set fill color and transparency
+       boxCR1pe->SetLineColor(kBlue);
+       boxCR1pe->SetLineWidth(2);
+       boxCR1pe->Draw();
+       Double_t CR2peTorelanceLow = TMath::Power(10, countRate2peLog10-countRate2peLog10Sigma);
+       Double_t CR2peTorelanceHigh = TMath::Power(10, countRate2peLog10+countRate2peLog10Sigma);
+       TBox *boxCR2pe = new TBox(0, CR2peTorelanceLow, 500, CR2peTorelanceHigh);
+       boxCR2pe->SetFillColorAlpha(kBlue, 0.3); // Set fill color and transparency
+       boxCR2pe->SetLineColor(kBlue);
+       boxCR2pe->SetLineWidth(2);
+       boxCR2pe->Draw();
+       Double_t CR3peTorelanceLow = TMath::Power(10, countRate3peLog10-countRate3peLog10Sigma);
+       Double_t CR3peTorelanceHigh = TMath::Power(10, countRate3peLog10+countRate3peLog10Sigma);
+       TBox *boxCR3pe = new TBox(0, CR3peTorelanceLow, 500, CR3peTorelanceHigh);
+       boxCR3pe->SetFillColorAlpha(kBlue, 0.3); // Set fill color and transparency
+       boxCR3pe->SetLineColor(kBlue);
+       boxCR3pe->SetLineWidth(2);
+       boxCR3pe->Draw();
+       
        lineCR1pe->SetLineColor(kBlue);
        lineCR1pe->Draw();
        lineCR2pe->SetLineColor(kBlue);
@@ -197,13 +265,16 @@ double analyzeRootFile(const char* fileName, Int_t canvasDimentionX, Int_t canva
        TLatex* textGain = new TLatex(200, 1e6, Form("Gain(%03i %02i %02i) = %4.2f", iCIRASAME, iASIC, iChannel, gain));
        textGain->Draw();
     }
-    
+    graph->SetTitle("Threshold Scan Scaler Count Rates");
     graph->GetXaxis()->SetLimits(0.0, 500.0);
     graph->GetHistogram()->SetMinimum(1.0);
     graph->GetHistogram()->SetMaximum(1e8);
+    graph->GetXaxis()->SetTitle("Threshold (DAC)");
+    graph->GetYaxis()->SetTitle("Counts");
     canvas->Print(outputPDFFileName);
     std::cout << std::endl;
-    
+
+    logFile.close();
     delete canvas;
     delete graph;
     delete hist;
@@ -236,7 +307,7 @@ void pulseheightfit_all(TString fileName) {
    outputPDFFileName = outputPDFDir + "/pulseheightfit_all_" + matchedPart + ".pdf";
    
    Int_t nCIRASAME = 18;
-   Int_t nASIC = 1;
+   Int_t nASIC = 4;
    Int_t nChannel = 32;
    
    if (!inputFile || inputFile->IsZombie()) {
@@ -253,10 +324,11 @@ void pulseheightfit_all(TString fileName) {
    delete dummyCanvas;
    
    TH1F* h_gain = new TH1F("h_gain", "Gain distribution", 100, 0, 100);
+   h_gain->GetXaxis()->SetTitle("Gain in ADC value");
+   h_gain->GetXaxis()->SetTitle("Count");
    for(int iCIRASAME=1; iCIRASAME<=nCIRASAME; ++iCIRASAME){
       for(int iASIC=1; iASIC<=nASIC; ++iASIC){
          for(int iChannel=1; iChannel<=nChannel; ++iChannel){
-//         for(int iChannel=7; iChannel<=7; ++iChannel){
             gain = analyzeRootFile(fileName, canvasDimentionX, canvasDimentionY,
                                     iCIRASAME, iASIC, iChannel, inputFile, outputPDFFileName);
             h_gain->Fill(gain);
