6ee7cc1befc8455fcde39d7cec71de30aacd61bd
rin1999 <okarinvc@gmail.com>
2024-05-13 17:41:03 +0900

    reflected deleted files

--- bin/format_all_cirasame_data.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for i in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18
-do
-echo ./format_all_asic_data.sh cirasame0$i\_$1 thresholdscan2024$1
-./format_all_asic_data.sh cirasame0$i\_$1 thresholdscan_2024$1
-done
--- bin/thresholdscan.cmd
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-python3 scaler_reader.py -n cirasame003_18night -s yaml_files/cirasame003/settings.yml
-python3 scaler_reader.py -n cirasame004_18night -s yaml_files/cirasame004/settings.yml
-python3 scaler_reader.py -n cirasame005_18night -s yaml_files/cirasame005/settings.yml
-python3 scaler_reader.py -n cirasame006_18night -s yaml_files/cirasame006/settings.yml
